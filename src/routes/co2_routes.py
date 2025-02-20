efbd2e46fec7300dbd709f5ee1f2b630b4c37c8e
Precious Nnorom <nnorompreciousakachukwu@gmail.com>
2025-02-20 18:11:50 +0100

    feat:Added /co2-data route to fetch and display CO2 data

--- src/routes/co2_routes.py
+++ src/routes/co2_routes.py
@@ -1,7 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from src.services.co2_service import fetch_co2_data
 
 co2_bp = Blueprint("co2", __name__)
 
 @co2_bp.route("/", methods=["GET"])
 def home():
     return "CO2 Monitoring API is running!"
+
+@co2_bp.route("/co2-data", methods=["GET"])
+def get_co2_data():
+    country_code = request.args.get('countryCode', 'US')  # Default to 'US' if not provided
+    data = fetch_co2_data(country_code)
+    if data:
+        return jsonify(data), 200
+    else:
+        return jsonify({"error": "Failed to fetch CO2 data"}), 500
